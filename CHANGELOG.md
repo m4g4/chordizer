d50863df8e60cf20018aa65305740983b807da4a
m4g4 <martin.gabor.sk@gmail.com>
2023-10-13 21:24:39 +0200

    Added mutation observer and new attribute chord-version

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+All important changes to this project will be documented here.
+This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.1.0] - 2023-10-13
+
+-   Added mutation observer to reflect changes on chord `div` declarations.
+-   Added `chord-version` attribute to define be able to better define different versions of the same chord.
+
+## [1.0.0]
+
+-   Initial version.
--- js/chordizer.js
+++ js/chordizer.js
@@ -1,15 +1,18 @@
-
-var Chordizer = (function ( $ ) {
-
+var Chordizer = (function ($) {
     var STRING_TONES = ["LowE", "A", "D", "G", "H", "HighE"];
 
-    function fretNumToName (fretNum) {
+    function fretNumToName(fretNum) {
         switch (fretNum) {
-            case 1: return "First";
-            case 2: return "Second";
-            case 3: return "Third";
-            case 4: return "Fourth";
-            case 5: return "Fifth";
+            case 1:
+                return "First";
+            case 2:
+                return "Second";
+            case 3:
+                return "Third";
+            case 4:
+                return "Fourth";
+            case 5:
+                return "Fifth";
         }
     }
 
@@ -21,34 +24,38 @@ var Chordizer = (function ( $ ) {
         if (Array.isArray(strings)) {
             var self = this;
             strings.forEach(function (str, i) {
+                var note =
+                    notes && Array.isArray(notes) && notes.length > i
+                        ? '<span class="Note">' + notes[i] + "</span>"
+                        : "";
 
-                var note = notes && Array.isArray(notes) && notes.length > i
-                    ? "<span class=\"Note\">"+notes[i]+"</span>"
-                    : '';
-
-                self.jqObject.find(".Fret.Zero ." +str)
-                             .append("<div class=\"OpenString\">"+note+"</div>");
+                self.jqObject
+                    .find(".Fret.Zero ." + str)
+                    .append('<div class="OpenString">' + note + "</div>");
             });
         } else {
-            var note = notes ? "<span class=\"Note\">"+notes+"</span>" : '';
-            this.jqObject.find(".Fret.Zero ." +strings)
-                         .append("<div class=\"OpenString\">"+note+"</div>");
+            var note = notes ? '<span class="Note">' + notes + "</span>" : "";
+            this.jqObject
+                .find(".Fret.Zero ." + strings)
+                .append('<div class="OpenString">' + note + "</div>");
         }
 
         return this;
-    }
+    };
 
     Fretboard.prototype.addMuteOne = function (str) {
-        this.jqObject.find(".Fret.Zero ." +str).append("<div class=\"Mute\">X</div>");
-        this.jqObject.find(".Fret.Previous ." +str).addClass("Muted");
-        this.jqObject.find(".Fret.First ." +str).addClass("Muted");
-        this.jqObject.find(".Fret.Second ." +str).addClass("Muted");
-        this.jqObject.find(".Fret.Third ." +str).addClass("Muted");
-        this.jqObject.find(".Fret.Fourth ." +str).addClass("Muted");
-        this.jqObject.find(".Fret.Fifth ." +str).addClass("Muted");
+        this.jqObject
+            .find(".Fret.Zero ." + str)
+            .append('<div class="Mute">X</div>');
+        this.jqObject.find(".Fret.Previous ." + str).addClass("Muted");
+        this.jqObject.find(".Fret.First ." + str).addClass("Muted");
+        this.jqObject.find(".Fret.Second ." + str).addClass("Muted");
+        this.jqObject.find(".Fret.Third ." + str).addClass("Muted");
+        this.jqObject.find(".Fret.Fourth ." + str).addClass("Muted");
+        this.jqObject.find(".Fret.Fifth ." + str).addClass("Muted");
 
         return this;
-    }
+    };
 
     Fretboard.prototype.addMute = function (strings) {
         if (Array.isArray(strings)) {
@@ -61,206 +68,261 @@ var Chordizer = (function ( $ ) {
         }
 
         return this;
-    }
+    };
 
     Fretboard.prototype.addFretPress = function (fretNum, strings, fingers) {
         var fretName = fretNumToName(fretNum);
         var self = this;
         if (Array.isArray(strings)) {
             strings.forEach(function (str, i) {
-                var finger = fingers && Array.isArray(fingers) && fingers.length > i ? "<span class=\"Finger\">"+fingers[i]+"</span>" : '';
-                self.jqObject.find(".Fret."+fretName+" ." +str).append("<div class=\"Press\">"+finger+"</div>");
+                var finger =
+                    fingers && Array.isArray(fingers) && fingers.length > i
+                        ? '<span class="Finger">' + fingers[i] + "</span>"
+                        : "";
+                self.jqObject
+                    .find(".Fret." + fretName + " ." + str)
+                    .append('<div class="Press">' + finger + "</div>");
             });
         } else {
-            var finger = fingers ? "<span class=\"Finger\">"+fingers+"</span>" : '';
-            this.jqObject.find(".Fret."+fretName+" ." +strings).append("<div class=\"Press\">"+finger+"</div>");
+            var finger = fingers
+                ? '<span class="Finger">' + fingers + "</span>"
+                : "";
+            this.jqObject
+                .find(".Fret." + fretName + " ." + strings)
+                .append('<div class="Press">' + finger + "</div>");
         }
 
         return this;
-    }
+    };
 
-    Fretboard.prototype.addBarre = function (fretNum, startString, endString, fingers) {
+    Fretboard.prototype.addBarre = function (
+        fretNum,
+        startString,
+        endString,
+        fingers
+    ) {
         var fretName = fretNumToName(fretNum);
 
         var startIndex = STRING_TONES.indexOf(startString);
         var endIndex = STRING_TONES.indexOf(endString);
 
         if (startIndex == -1 || endIndex == -1) {
-            console.error("Cannot add barre chord. Invalid string name " + startString + ":" + endString);
+            console.error(
+                "Cannot add barre chord. Invalid string name " +
+                    startString +
+                    ":" +
+                    endString
+            );
         }
 
         var tones = [];
-        for (var i = Math.min(startIndex, endIndex) + 1; i < Math.max(startIndex, endIndex); i++) {
+        for (
+            var i = Math.min(startIndex, endIndex) + 1;
+            i < Math.max(startIndex, endIndex);
+            i++
+        ) {
             tones.push(STRING_TONES[i]);
         }
 
-        var fingersStartElem = fingers && Array.isArray(fingers)
-            ? "<span class=\"Finger\">"+fingers[0]+"</span>"
-            : '';
-        var fingersEndElem = fingers && Array.isArray(fingers)
-            ? "<span class=\"Finger\">"+fingers[fingers.length - 1]+"</span>"
-            : '';
+        var fingersStartElem =
+            fingers && Array.isArray(fingers)
+                ? '<span class="Finger">' + fingers[0] + "</span>"
+                : "";
+        var fingersEndElem =
+            fingers && Array.isArray(fingers)
+                ? '<span class="Finger">' +
+                  fingers[fingers.length - 1] +
+                  "</span>"
+                : "";
 
-        this.jqObject.find(".Fret."+fretName+" ." +startString)
-                 .append("<div class=\"Barre Start\">"+fingersStartElem+"</div>");
-        this.jqObject.find(".Fret."+fretName+" ." +endString)
-                 .append("<div class=\"Barre End\">"+fingersEndElem+"</div>");
+        this.jqObject
+            .find(".Fret." + fretName + " ." + startString)
+            .append('<div class="Barre Start">' + fingersStartElem + "</div>");
+        this.jqObject
+            .find(".Fret." + fretName + " ." + endString)
+            .append('<div class="Barre End">' + fingersEndElem + "</div>");
 
         var self = this;
         tones.forEach(function (t, i) {
+            if (
+                fingers &&
+                !Array.isArray(fingers) &&
+                i === Math.floor(tones.length / 2)
+            ) {
+                var fingerElem =
+                    fingers && !Array.isArray(fingers)
+                        ? '<span class="Finger">' + fingers + "</span>"
+                        : "";
 
-            if (fingers && !Array.isArray(fingers) && i === Math.floor(tones.length / 2)) {
-                var fingerElem = fingers && !Array.isArray(fingers)
-                    ? "<span class=\"Finger\">"+fingers+"</span>"
-                    : '';
-
-                self.jqObject.find(".Fret."+fretName+" ." +t)
-                         .append("<div class=\"Barre\">"+fingerElem+"</div>");
-
+                self.jqObject
+                    .find(".Fret." + fretName + " ." + t)
+                    .append('<div class="Barre">' + fingerElem + "</div>");
             } else if (Array.isArray(fingers)) {
-                self.jqObject.find(".Fret."+fretName+" ." +t)
-                         .append("<div class=\"Barre\"><span class=\"Finger\">"+fingers[i+1]+"</span></div>");
-
+                self.jqObject
+                    .find(".Fret." + fretName + " ." + t)
+                    .append(
+                        '<div class="Barre"><span class="Finger">' +
+                            fingers[i + 1] +
+                            "</span></div>"
+                    );
             } else {
-                self.jqObject.find(".Fret."+fretName+" ." +t)
-                         .append("<div class=\"Barre\"></div>");
+                self.jqObject
+                    .find(".Fret." + fretName + " ." + t)
+                    .append('<div class="Barre"></div>');
             }
         });
 
         return this;
-    }
+    };
 
-    document.addEventListener('DOMContentLoaded', function() {
-
-        $(".chordizer").each( function( i, el ) {
-            var elem = $( el );
-            if (typeof elem.attr("custom") !== 'undefined') {
-                return;
-            }
-
-            if (typeof elem.data("chord") === 'undefined') {
-                console.error("Cannot render chord element. Attribute data-chord missing!");
-                return;
-            }
-
-            var chordSymbol = elem.data("chord");
-
-            var attributes = {};
-
-            if (typeof elem.attr("small") !== 'undefined') {
-                attributes['small'] = true;
-            } else if (typeof elem.attr("large") !== 'undefined') {
-                attributes['large'] = true;
-            }
-
-            if (typeof elem.data("title") !== 'undefined') {
-                attributes['customTitle'] = elem.data("title");
-            }
-
-            Chordizer.addChordBySymbol(elem, chordSymbol, attributes);
-        });
-
+    document.addEventListener("DOMContentLoaded", function () {
+        initialize();
     });
 
-    var addChordBySymbol = function (chordizerElement, chordSymbol, attributes) {
+    var mutationCallback = function (mutationsList) {
+        for (var i = 0; i < mutationsList.length; i++) {
+            var mutation = mutationsList[i];
+            $(mutation.target).empty();
+            initializeChord(mutation.target);
+        }
+    };
 
-        chordSymbol = chordSymbol
-            .replace('#', 'is')
-            .replace('/', '_');
+    var mutationObserver = new MutationObserver(mutationCallback);
+
+    var initializeChord = function (element) {
+        var elem = $(element);
+        if (typeof elem.attr("custom") !== "undefined") {
+            return;
+        }
+
+        if (typeof elem.attr("data-chord") === "undefined") {
+            console.error(
+                "Cannot render chord element. Attribute data-chord missing!"
+            );
+            return;
+        }
+
+        var chordSymbol = elem.attr("data-chord");
+
+        var attributes = {};
+
+        if (typeof elem.attr("small") !== "undefined") {
+            attributes["small"] = true;
+        } else if (typeof elem.attr("large") !== "undefined") {
+            attributes["large"] = true;
+        }
+
+        attributes["chord-version"] = elem.attr("chord-version");
+
+        if (typeof elem.attr("data-title") !== "undefined") {
+            attributes["customTitle"] = elem.attr("data-title");
+        }
+
+        addChordBySymbol(elem, chordSymbol, attributes);
+    };
+
+    var initialize = function () {
+        $(".chordizer").each(function (_, element) {
+            initializeChord(element);
+            mutationObserver.observe(element, { attributes: true });
+        });
+    };
+
+    var addChordBySymbol = function (
+        chordizerElement,
+        chordSymbol,
+        attributes
+    ) {
+        chordSymbol = chordSymbol.replace("#", "is").replace("/", "_");
+        var version = attributes["chord-version"];
+        if (version && Chordizer[chordSymbol + "v" + version]) {
+            chordSymbol = chordSymbol + "v" + version;
+        }
 
         if (!Chordizer[chordSymbol]) {
             console.error("Chord symbol " + chordSymbol + " does not exist!");
             return;
         }
         Chordizer[chordSymbol](chordizerElement, attributes);
-    }
+    };
 
-    var addCustomChord = function (parentElementId, chordTitle, fretNumber, attributes) {
-        return this.addChord($("#" + parentElementId), chordTitle, fretNumber, attributes);
-    }
+    var addCustomChord = function (
+        parentElementId,
+        chordTitle,
+        fretNumber,
+        attributes
+    ) {
+        return this.addChord(
+            $("#" + parentElementId),
+            chordTitle,
+            fretNumber,
+            attributes
+        );
+    };
 
-    var addChord = function (parentElement, chordTitle, fretNumber, attributes) {
-
+    var addChord = function (
+        parentElement,
+        chordTitle,
+        fretNumber,
+        attributes
+    ) {
         if (!attributes) {
             attributes = {};
         }
 
-        var chordSymbol = chordTitle
-            .replace('#', 'is')
-            .replace('/', '_');
+        var chordSymbol = chordTitle.replace("#", "is").replace("/", "_");
 
-        var extraClass = attributes['hasFiveFrets'] ? " hasFiveFrets" : "";
-        var fifthFretRow = attributes['hasFiveFrets'] ?
-            "<tr class=\"Fret Fifth\">\
-                <td class=\"LowE\"></td>\
-                <td class=\"A\"></td>\
-                <td class=\"D\"></td>\
-                <td class=\"G\"></td>\
-                <td class=\"H\"></td>\
-                <td class=\"HighE\"></td>\
-            </tr>"
-        : "";
+        var fretTds =
+            '\
+            <td class="LowE"></td >\
+            <td class="A"></td>\
+            <td class="D"></td>\
+            <td class="G"></td>\
+            <td class="H"></td>\
+            <td class="HighE"></td>\
+        ';
 
-        extraClass += attributes['small'] ? " small" : "";
-        extraClass += attributes['large'] ? " large" : "";
+        var extraClass = attributes["hasFiveFrets"] ? " hasFiveFrets" : "";
+        var fifthFretRow = attributes["hasFiveFrets"]
+            ? '<tr class="Fret Fifth">' + fretTds + "</tr>"
+            : "";
 
-        if (attributes['customTitle']) {
-            chordName = attributes['customTitle'];
+        extraClass += attributes["small"] ? " small" : "";
+        extraClass += attributes["large"] ? " large" : "";
+
+        if (attributes["customTitle"]) {
+            chordName = attributes["customTitle"];
         }
 
         parentElement.append(
-            "<div class=\"chord " + chordSymbol + extraClass + "\">\
+            '<div class="chord ' +
+                chordSymbol +
+                extraClass +
+                '">\
             <table>\
-            <tbody><tr class=\"Fret Zero\">\
-                <td class=\"LowE\"></div></td>\
-                <td class=\"A\"></td>\
-                <td class=\"D\"></td>\
-                <td class=\"G\"></td>\
-                <td class=\"H\"></td>\
-                <td class=\"HighE\"></td>\
-            </tr>\
-            <tr class=\"Fret Previous\">\
-                <td class=\"LowE\"></div></td>\
-                <td class=\"A\"></td>\
-                <td class=\"D\"></td>\
-                <td class=\"G\"></td>\
-                <td class=\"H\"></td>\
-                <td class=\"HighE\"></td>\
-            </tr>\
-            <tr class=\"Fret First\">\
-                <td class=\"LowE\"></td>\
-                <td class=\"A\"></td>\
-                <td class=\"D\"></td>\
-                <td class=\"G\"></td>\
-                <td class=\"H\"></td>\
-                <td class=\"HighE\"></td>\
-            </tr>\
-            <tr class=\"Fret Second\">\
-                <td class=\"LowE\"></td>\
-                <td class=\"A\"></td>\
-                <td class=\"D\"></td>\
-                <td class=\"G\"></td>\
-                <td class=\"H\"></td>\
-                <td class=\"HighE\"></td>\
-            </tr>\
-            <tr class=\"Fret Third\">\
-                <td class=\"LowE\"></td>\
-                <td class=\"A\"></td>\
-                <td class=\"D\"></td>\
-                <td class=\"G\"></td>\
-                <td class=\"H\"></td>\
-                <td class=\"HighE\"></td>\
-            </tr>\
-            <tr class=\"Fret Fourth\">\
-                <td class=\"LowE\"></td>\
-                <td class=\"A\"></td>\
-                <td class=\"D\"></td>\
-                <td class=\"G\"></td>\
-                <td class=\"H\"></td>\
-                <td class=\"HighE\"></td>\
-            </tr>" + fifthFretRow +
-            "</tbody></table>\
-            <div class=\"ChordName\">" + chordTitle + "</div>\
+            <tbody><tr class="Fret Zero">' +
+                fretTds +
+                '</tr>\
+            <tr class="Fret Previous">' +
+                fretTds +
+                '</tr>\
+            <tr class="Fret First">' +
+                fretTds +
+                '</tr>\
+            <tr class="Fret Second">' +
+                fretTds +
+                '</tr>\
+            <tr class="Fret Third">' +
+                fretTds +
+                '</tr>\
+            <tr class="Fret Fourth">' +
+                fretTds +
+                "</tr>" +
+                fifthFretRow +
+                '</tbody></table>\
+            <div class="ChordName">' +
+                chordTitle +
+                "</div>\
             </div>"
         );
 
@@ -269,16 +331,17 @@ var Chordizer = (function ( $ ) {
         if (fretNumber <= 1) {
             chordElement.addClass("FromZeroFret");
         } else {
-            chordElement.prepend("<div class=\"FretNum\">" + fretNumber + ".</div>");
+            chordElement.prepend(
+                '<div class="FretNum">' + fretNumber + ".</div>"
+            );
         }
 
         return new Fretboard(chordElement);
-    }
+    };
 
     return {
         addChordBySymbol: addChordBySymbol,
         addCustomChord: addCustomChord,
-        addChord: addChord
+        addChord: addChord,
     };
-
-})( jQuery );
+})(jQuery);
